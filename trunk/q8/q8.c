1e976e6be6fe81161025aef308e9ad36dc893ae7
marcosifpe@gmail.com <marcosifpe@gmail.com@c1c54dcc-ae75-b6ee-9c04-2d5cb8fdd448>
2013-11-07 01:54:41 +0000

    Ajustando formatação do código

--- trunk/q8/q8.c
+++ trunk/q8/q8.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-int main(void){
+int main(void) {
 	freopen("e8.txt", "r", stdin);
 	freopen("s8.txt", "w", stdout);
-	
-	srand(time(NULL));
-	int a,b,c,i,j,k;
-	scanf("%d",&j);
-for(k=0;k<j;k++){
 
-i=(rand()%900 +100);
-a=i/100;
-b=(i%100-i%10)/10;
-c=i%10;
-printf("%d %d%d%d\n",i,c,b,a);
-}
- 	fclose(stdin);
-    fclose(stdout);
+	srand(time(NULL));
+	int a, b, c, i, j, k;
+	scanf("%d", &j);
+	for (k = 0; k < j; k++) {
+
+		i = (rand() % 900 + 100);
+		a = i / 100;
+		b = (i % 100 - i % 10) / 10;
+		c = i % 10;
+		printf("%d %d%d%d\n", i, c, b, a);
+	}
+	fclose(stdin);
+	fclose(stdout);
 	return 0;
 }
